0bb224300c7aa74934ad98e9fa9d597d9997a291
Xmchx <1394466835@qq.com>
2025-04-28 11:08:26 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme github_light")
-  end,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
 }
